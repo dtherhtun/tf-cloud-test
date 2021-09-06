971a1f9782fb4384e4f55cab53e39b0e1d728eeb
D Ther <dtherhtun.cw@gmail.com>
2021-09-06 20:58:10 +0630

    first commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.26.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  required_version = ">= 0.14"
+
+  backend "remote" {
+    organization = "k8smm"
+
+    workspaces {
+      name = "tf-cloud-test"
+    }
+  }
+}
+
