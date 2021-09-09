b26845db9e94a133e0001b2bc8af74c85614efa9
D Ther <dtherhtun.cw@gmail.com>
2021-09-09 21:53:14 +0630

    create vpc

--- main.tf
+++ main.tf
@@ -1,25 +1,33 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.26.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.1"
-    }
-  }
-  required_version = ">= 0.14"
-
-  backend "remote" {
-    organization = "k8smm"
-
-    workspaces {
-      name = "tf-cloud-test"
-    }
-  }
+locals {
+  project_name = "lts"
+  owner        = "Platform-team"
+  region       = "us-east-1"
+  subnet       = chunklist([for x in cidrsubnets("10.0.0.0/8", 8, 8, 8, 8, 8, 8, 8, 8, 8, 8) : x if x != cidrsubnets("10.0.0.0/8", 8, 8, 8, 8)[0]], 3)
 }
 
-provider "aws" {
-  region = "us-west-2"
+# create vpc from here
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.7.0"
+
+  name = join("-", [local.project_name, "vpc"])
+  cidr = var.vpc.cidr
+
+  azs              = [for x in ["a", "b", "c"] : "${local.region}${x}"]
+  private_subnets  = subnet[0]
+  public_subnets   = subnet[1]
+  database_subnets = subnet[2]
+
+  enable_nat_gateway     = var.vpc.is_enable_natgw
+  enable_vpn_gateway     = var.vpc.is_enable_vpngw
+  single_nat_gateway     = var.vpc.is_single_natgw
+  one_nat_gateway_per_az = var.vpc.is_one_natgw_per_az
+
+  create_database_subnet_group       = var.vpc.is_create_db_sub_grp
+  create_database_subnet_route_table = var.vpc.is_create_db_sub_rt
+
+  tags = {
+    Name  = local.project_name
+    Owner = local.owner
+  }
 }
---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "vpc" {
+  type = object({
+    is_enable_vpngw      = bool
+    is_enable_natgw      = bool
+    is_single_natgw      = bool
+    is_one_natgw_per_az  = bool
+    is_create_db_sub_grp = bool
+    is_create_db_sub_rt  = bool
+  })
+  default = {
+    is_enable_vpngw      = false
+    is_enable_natgw      = true
+    is_single_natgw      = true
+    is_one_natgw_per_az  = false
+    is_create_db_sub_grp = true
+    is_create_db_sub_rt  = true
+  }
+  description = <<EOF
+  Group variables of aws vpc looks like this:
+  ```
+  vpc = {
+    is_enable_vpngw      = false
+    is_enable_natgw      = true
+    is_single_natgw      = true
+    is_one_natgw_per_az  = false
+    is_create_db_sub_grp = true
+    is_create_db_sub_rt  = true
+  }```
+  EOF
+}
